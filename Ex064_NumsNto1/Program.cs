3f615af4be72af8a48b7c382e00ba4df4ded086a
Andrey Khudyakov <avh_2004@mail.ru>
2022-11-30 07:58:35 +0500

    Added Exersize №64 Numbers from N to 1

---
+++ Ex064_NumsNto1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные 
+// числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int N = GetNaturalNumber("Введите целое число больше 1:");
+ShowNumsNto1(N);
+
+int GetNaturalNumber(string msg)
+{
+    Console.WriteLine(msg);
+    int value;
+    while (!int.TryParse(Console.ReadLine(), out value) || value <= 1)
+    {
+        Console.WriteLine("Ошибка!");
+        Console.WriteLine(msg);
+    }
+    return value;
+}
+
+void ShowNumsNto1(int number)
+{
+    Console.Write(number + ", ");
+    number--; 
+    if (number > 1) ShowNumsNto1(number);
+    else Console.Write(1);
+}
